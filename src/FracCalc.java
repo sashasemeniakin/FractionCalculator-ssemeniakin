9c4ebda4cf43757e6450db2864028f3039481599
sashasemeniakin <552057@my.cuhsd.org>
2022-12-22 15:59:16 -0800

    final

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 
+
+/**
+ * Fractional Calculator class
+ */
 public class FracCalc {
 
-    public static void main(String[] args) 
+
+    /**
+     * main method
+     * @param args command line args
+     */
+    public static void main(String[] args)
     {
         //Read the input from the user and call produceAnswer with an equation
         // Create a Scanner for user input
@@ -25,63 +34,140 @@ public class FracCalc {
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
+    /**
+     * function to parse input and produce the answer
+     *  // This function takes a String 'input' and produces the result
+     *     //
+     *     // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
+     *     //      e.g. input ==> "1/2 + 3/4"
+     *     //
+     *     // The function should return the result of the fraction after it has been calculated
+     *     //      e.g. return ==> "1_1/4"
+     * @param input String input with operands and operator
+     * @return proper function in String format
+     */
     public static String produceAnswer(String input)
     {
         //split input on space
         String[] splited = input.split("\\s+");
-        //if input doesn't have 3 components, error out
-        if (splited.length != 3) {
-            throw new RuntimeException("invalid number of operands");
-        }
-        //read first and second fraction input and operator
-        String first = splited[0];
-        String operator = splited[1];
-        String second = splited[2];
 
-        //create freactions from input strings
-        Fraction firstFraction = new Fraction(first);
-        Fraction secondFraction = new Fraction(second);
+        int index = 0;
+        String first;
+        String operator;
+        String second;
+        String resultString = null;
+        while(index < splited.length-1) {
+            if(index == 0){
+                //we're just starting so read first and second fraction input and operator
+                first = splited[0];
+                operator = splited[1];
+                second = splited[2];
+                index = 2;
+            } else {
+                //we're already looked at numbers before index
+                //first is the result of previous loop
+                first = resultString;
+                operator = splited[index + 1];
+                second = splited[index + 2];
+                index = index + 2;
+            }
 
-        Fraction result;
-        //convert fractions to improper fractions
-        firstFraction.convertToimproper();
-        secondFraction.convertToimproper();
+            //create fractions from input strings
+            Fraction firstFraction = new Fraction(first);
+            Fraction secondFraction = new Fraction(second);
 
-        if(operator.contains("+")){
-            // if the operation is addition it should cross multiply the fractions and then divide the numerator by denominator
-            int numerator = (firstFraction.getNumerator()* secondFraction.getDenominator())+(secondFraction.getNumerator()* firstFraction.getDenominator());
-            int denominator = (firstFraction.getDenominator()* secondFraction.getDenominator());
-            result = new Fraction(numerator,denominator);
-        } else if (operator.contains ("-")) {
-            //if operator is subtraction,
-            int numerator = (secondFraction.getDenominator()* firstFraction.getNumerator()-firstFraction.getDenominator()* secondFraction.getNumerator()); //numerator
-            int denominator = (firstFraction.getDenominator()* secondFraction.getDenominator()); //denominator
-            result = new Fraction(numerator,denominator);
-        } else if (operator.contains ("*")) {
-            //if operator is multiplication
-            int numerator = (firstFraction.getNumerator()* secondFraction.getNumerator());
-            int denominator = (firstFraction.getDenominator()* secondFraction.getDenominator());
-            result = new Fraction(numerator,denominator);
-        }else if (operator.contains ("/")) {
-            //if operator is division
-            int numerator = (firstFraction.getNumerator()* secondFraction.getDenominator());
-            int denominator = (secondFraction.getNumerator()* firstFraction.getDenominator());
-            result = new Fraction(numerator,denominator);
-        }else{
-            //unknown operator
-            throw new RuntimeException("invalid operator");
+            Fraction resultFraction;
+            //convert fractions to improper fractions
+            firstFraction.convertToimproper();
+            secondFraction.convertToimproper();
+
+            if (operator.contains("+")) {
+                // if the operation is addition it should cross multiply the fractions and then divide the numerator by denominator
+                int numerator = (firstFraction.getNumerator() * secondFraction.getDenominator()) + (secondFraction.getNumerator() * firstFraction.getDenominator());
+                int denominator = (firstFraction.getDenominator() * secondFraction.getDenominator());
+                resultFraction = new Fraction(numerator, denominator);
+            } else if (operator.contains("-")) {
+                //if operator is subtraction,
+                int numerator = (secondFraction.getDenominator() * firstFraction.getNumerator() - firstFraction.getDenominator() * secondFraction.getNumerator()); //numerator
+                int denominator = (firstFraction.getDenominator() * secondFraction.getDenominator()); //denominator
+                resultFraction = new Fraction(numerator, denominator);
+            } else if (operator.contains("*")) {
+                //if operator is multiplication
+                int numerator = (firstFraction.getNumerator() * secondFraction.getNumerator());
+                int denominator = (firstFraction.getDenominator() * secondFraction.getDenominator());
+                resultFraction = new Fraction(numerator, denominator);
+            } else if (operator.contains("/")) {
+                //if operator is division
+                int numerator = (firstFraction.getNumerator() * secondFraction.getDenominator());
+                int denominator = (secondFraction.getNumerator() * firstFraction.getDenominator());
+                resultFraction = new Fraction(numerator, denominator);
+            } else {
+                //unknown operator
+                throw new RuntimeException("invalid operator");
+            }
+            resultString = simplifyFraction(resultFraction);
         }
         //resturn result without simplifying
-        return result.getNumerator()+"/"+result.getDenominator();
+        return resultString;
     }
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
-    
+    /**
+     * simplify fraction
+     * @param fraction Fraction object to simplify
+     * @return String representation of the simplified fraction
+     */
+    static String simplifyFraction(Fraction fraction) {
+        //divide numerator by denominator and take the whole from it.
+        int whole = fraction.getNumerator() / fraction.getDenominator();
+        // You then take the denominator and mupltiply by the whole
+        int newnum = fraction.getDenominator() * whole;
+        // subtract that number from numerator.
+        int numerator = fraction.getNumerator() - newnum;
+
+        if (numerator==0){
+            return whole+"";
+        }
+
+        int greatestCommonDenomator = Math.abs(findGreatestCommonDenominator (numerator, fraction.getDenominator()));
+        int resultNumerator = numerator / greatestCommonDenomator;
+        int resultDenominator  = fraction.getDenominator() / greatestCommonDenomator;
+
+        boolean answerIsNegative = false;
+        if((resultDenominator < 0 && resultNumerator >0) ||
+                (resultDenominator>0 && resultNumerator<0)) {
+            answerIsNegative = true;
+        }
+        if (resultNumerator<0) {
+            resultNumerator = resultNumerator*-1;
+        }
+        if(resultDenominator < 0) {
+            resultDenominator = resultDenominator * -1;
+        }
+
+        if(whole == 0) {
+           if(answerIsNegative) {
+               return "-"+resultNumerator +"/"+ resultDenominator;
+           } else {
+               return resultNumerator + "/" + resultDenominator;
+           }
+        }
+
+
+        return whole +"_"+ resultNumerator +"/"+ resultDenominator;
+    }
+
+    /**
+     * function to find greatest common denominator
+     * @param a first parameter, denominator at the first pass
+     * @param b second parameter, numerator at the first pass
+     * @return greatest common denominator
+     */
+    static int findGreatestCommonDenominator(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return findGreatestCommonDenominator(b, a % b);
+    }
+
+
 }
--- src/Fraction.java
+++ src/Fraction.java
@@ -1,15 +1,24 @@
-//class to represent fraction
+
+/**
+ * class to represent fraction
+ */
 public class Fraction {
     private int whole = 0;
     private int numerator = 0;
     private int denominator = 1;
 
-    //constructor
+    /**
+     * constructor that creates fraction object given numerator and denominator
+     * @param numerator numerator of the fraction
+     * @param denominator denominator of the fraction
+     */
     public Fraction(int numerator,int denominator){
         this.numerator=numerator;
         this.denominator=denominator;
     }
-    //convert proper fraction to improper
+    /**
+     * convert proper fraction to improper
+     */
     public void convertToimproper(){
         //if there is no whole number, don't do anuything and return
         if (whole==0){
@@ -27,7 +36,11 @@ public class Fraction {
     }
 
 
-    //constructor to create fraction from string input
+    /**
+     * constructor to create fraction from string input
+     * @param input string input
+     * throws NumberFormatException on invalid format, zero denominator
+     */
     public Fraction(String input) {
         String[] firstSplited = input.split("_");
         //check length of firstSplited, if length is 1, then whole is not specified and if length is 2, whole is specified.
@@ -41,7 +54,7 @@ public class Fraction {
                     numerator = Integer.parseInt(firstFraction[0]);
                     denominator = Integer.parseInt(firstFraction[1]);
                 } else {
-                    throw new NumberFormatException("invalid fraction");
+                    throw new NumberFormatException("ERROR: Input is in invalid format");
                 }
 
             } else {
@@ -60,16 +73,30 @@ public class Fraction {
                 numerator = Integer.parseInt(firstFraction[0]);
                 denominator = Integer.parseInt(firstFraction[1]);
             } else {
-                throw new NumberFormatException("invalid fraction");
+                throw new NumberFormatException("ERROR: Input is in invalid format");
             }
         } else {
-            throw new NumberFormatException("invalid number format");
+            throw new NumberFormatException("ERROR: Input is in invalid format");
+        }
+
+        if(denominator == 0) {
+            throw new NumberFormatException("ERROR: Cannot divide by zero");
         }
 
     }
+
+    /**
+     * get whole part of the fraction
+     * @return whole part of th fraction
+     */
     public int getWhole(){
         return whole;
     }
+
+    /**
+     *
+     * @return
+     */
     public int getNumerator(){
         return numerator;
     }
